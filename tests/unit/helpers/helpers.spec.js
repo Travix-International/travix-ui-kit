d175eff9ad0e2af69565be1ebd31877c10a516cd
Alexandr Dudar <alexandr.dudar@gmail.com>
2017-02-14 13:37:47 +0100

    Updated mods helper and styles names according to the new BEM standards (#13)

--- tests/unit/helpers/helpers.spec.js
+++ tests/unit/helpers/helpers.spec.js
@@ -3,10 +3,10 @@ const getClassNamesWithMods = require('../../../components/_helpers.js').getClas
 describe('helpers', () => {
   describe('#getClassNamesWithMods()', () => {
     it('#1 should return class with mods for strings as class and array as mods', () => {
-      expect(getClassNamesWithMods('test', ['first', 'second'])).toBe('test test--first test--second');
+      expect(getClassNamesWithMods('test', ['first', 'second'])).toBe('test test_first test_second');
     });
     it('#2 should return class with mods for strings as array of classes and array as mods', () => {
-      expect(getClassNamesWithMods(['test', 'test2'], ['first', 'second'])).toBe('test test2 test--first test--second');
+      expect(getClassNamesWithMods(['test', 'test2'], ['first', 'second'])).toBe('test test2 test_first test_second');
     });
   });
 });
