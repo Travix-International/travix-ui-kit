53783244ccf7efc0bec01b1a8171ad72742e978c
Ricardo Machado <rmachado@travix.com>
2017-02-17 18:58:20 +0100

    Updates tests to match recent changes

--- tests/unit/price/price.spec.js
+++ tests/unit/price/price.spec.js
@@ -1,9 +1,36 @@
 import * as enzyme from 'enzyme';
 import * as enzymeToJson from 'enzyme-to-json';
 import React from 'react';
-import Price from '../../../components/price/price';
+import Price, { addThousandsSeparator, ensureDecimalPrecision } from '../../../components/price/price';
 
-describe('Button', () => {
+describe('Price', () => {
+  describe('#addThousandsSeparator', () => {
+    it('returns original value when thousandsSeparator is undefined || value.length < 3', () => {
+      expect(addThousandsSeparator('5000')).toEqual('5000');
+      expect(addThousandsSeparator('500', '.')).toEqual('500');
+    });
+
+    it('returns value with given thousandsSeparator', () => {
+      expect(addThousandsSeparator('5000', '.')).toEqual('5.000');
+      expect(addThousandsSeparator('50000', '.')).toEqual('50.000');
+      expect(addThousandsSeparator('50000', ' ')).toEqual('50 000');
+      expect(addThousandsSeparator('5000000', ' ')).toEqual('5 000 000');
+    });
+  });
+
+  describe('#ensureDecimalPrecision', () => {
+    it('returns truncated value to decimalsPrecision number when value.length > decimalsPrecision', () => {
+      expect(ensureDecimalPrecision('5000')).toEqual('50');
+      expect(ensureDecimalPrecision('5000', 3)).toEqual('500');
+    });
+
+    it('returns value suffixed by 0s value.length < decimalsPrecision until matches decimalsPrecision length', () => {
+      expect(ensureDecimalPrecision('5')).toEqual('50');
+      expect(ensureDecimalPrecision('5', 3)).toEqual('500');
+      expect(ensureDecimalPrecision('', 3)).toEqual('000');
+    });
+  });
+
   describe('#render()', () => {
     const shallow = enzyme.shallow;
     const shallowToJson = enzymeToJson.shallowToJson;
