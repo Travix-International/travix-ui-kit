64bc77053ddec54914cea69106cef787d27da5da
Ivana Viranovska <iviranovska@travix.com>
2017-02-21 17:49:28 +0100

    added stateless radioButton component

---
+++ components/radioButton/radioButton.md
@@ -0,0 +1,8 @@
+Radio Button:
+
+    <div>
+      <RadioButton />
+      <RadioButton children="test 1" defaultChecked id="radio_1" name="testRadioButton1" />
+      <RadioButton children={<span>with span</span>} id="radio_2" name="testRadioButton2" onChange={alert} />
+      <RadioButton disabled id="radio_3" name="testRadioButton3" onChange={alert}>disabled </RadioButton>
+    </div>
---
+++ tests/unit/radioButton/radioButton.spec.js
@@ -0,0 +1,42 @@
+import * as enzyme from 'enzyme';
+import * as enzymeToJson from 'enzyme-to-json';
+import React from 'react';
+import RadioButton from '../../../components/radioButton/radioButton';
+
+describe('Radio Button', () => {
+  describe('#render()', () => {
+    const shallow = enzyme.shallow;
+    const shallowToJson = enzymeToJson.shallowToJson;
+    const onChange = () => {};
+
+    it('should render disabled radio button', () => {
+      const wrapper = shallow(
+        <RadioButton disabled id="disabledRadio" onChange={onChange}>
+          Disabled radio button
+        </RadioButton>
+      );
+
+      expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render default checked radio button', () => {
+      const wrapper = shallow(
+        <RadioButton checked id="radio1" onChange={onChange}>
+          Radio 1
+        </RadioButton>
+      );
+
+      expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render radio button', () => {
+      const wrapper = shallow(
+        <RadioButton checked id="radio1" onChange={onChange}>
+          Radio 1
+        </RadioButton>
+      );
+
+      expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
+  });
+});
