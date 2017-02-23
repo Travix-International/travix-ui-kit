fa3752acfd510bf82b98889a9f931141f211b314
Ivana Viranovska <iviranovska@travix.com>
2017-02-23 14:34:42 +0100

    update test snapshots

--- components/index.js
+++ components/index.js
@@ -1,9 +1,11 @@
 import Button from './button/button';
+import Price from './price/price';
 import RadioButton from './radioButton/radioButton';
 import Spinner from './spinner/spinner';
 
 export default {
   Button,
+  Price,
   RadioButton,
   Spinner,
 };
