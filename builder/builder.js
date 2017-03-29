c107e1b57e182a68629bb2fd6ff73aaeb5ac9d88
Eduard_Trutsyk <ediktrutsik@gmail.com>
2017-03-29 17:23:14 +0300

    refactored builder

--- builder/builder.js
+++ builder/builder.js
@@ -1,24 +1,42 @@
-const getStylesAndSaveTheme = require('./getStylesAndSaveTheme');
+const path = require('path');
+const themeBuilder = require('theme-builder');
+
+const saveThemeScssFile = require('./saveThemeScssFile');
 const runWebpackAndCopyFilesToFinalDestination = require('./runWebpackAndCopyFilesToFinalDestination');
 const webpackConfig = require('./webpack.config');
 
+const defaultThemeYamlPath = path.join(__dirname, '..', 'themes', '_default.yaml');
+const defaultOutputThemeFile = path.join(__dirname, '..', 'themes', 'theme.scss');
 /**
  * Triggers the build process.
  *
  * @module builder
  * @param {String}  cssDir     Destination folder for the ui-bundle.css
  * @param {String}  jsDir      Destination folder for the ui-bundle.js
- * @param {String}  themeFile  Path where a custom YAML w/ styles' definitions
+ * @param {Array, String}  themeFile  Path where a custom YAML w/ styles' definitions
  * @param {Boolean} watch      Flag to determine if it should run in 'watch' mode
  * @return {Promise}
  */
-module.exports = ({ cssDir, environment, jsDir, themeFile, watch }) => {
-  return getStylesAndSaveTheme(themeFile, watch)
+module.exports = ({ cssDir, environment = 'development', output = defaultOutputThemeFile, jsDir, themeFile, watch }) => {
+  let themeFiles = [defaultThemeYamlPath];
+  if (themeFile) {
+    themeFiles = themeFiles.concat(themeFile);
+  }
+
+  const uiKitBuilder = themeBuilder({
+    format: 'scss',
+    prefix: 'tx',
+  });
+
+  return uiKitBuilder
+    .merge(themeFiles)
+    .then(uiKitBuilder.build)
+    .then(result => saveThemeScssFile(output, result))
     .then(() => runWebpackAndCopyFilesToFinalDestination({
       cssDir,
       jsDir,
       watch,
       webpackConfig,
-      webpackNodeEnv: { 'process.env.NODE_ENV': environment || 'development' },
+      webpackNodeEnv: { 'process.env.NODE_ENV': environment },
     }));
 };
--- builder/saveThemeScssFile.js
+++ builder/saveThemeScssFile.js
@@ -1,13 +1,12 @@
 const fs = require('fs');
-const path = require('path');
 
 /**
  * @module saveThemeScssFile
  * @param {String} themeScss SCSS content to be stored as themes/theme.scss
  * @return {Promise}
  */
-module.exports = themeScss => new Promise((resolve, reject) => {
-  fs.writeFile(path.join(__dirname, '..', 'themes', 'theme.scss'), themeScss, (err) => {
+module.exports = (file, themeScss) => new Promise((resolve, reject) => {
+  fs.writeFile(file, themeScss, (err) => {
     if (err) {
       reject(err);
       return;
