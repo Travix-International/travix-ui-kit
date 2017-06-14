217d2d2f9cd5e960e06b62d63b3ff7de446ba0f2
Sergey Maksimuk <sergey_maksimuk@epam.com>
2017-06-14 12:59:45 +0300

    implemented Badge component

---
+++ components/badge/badge.js
@@ -0,0 +1,78 @@
+import React, {
+  Component,
+  PropTypes,
+} from 'react';
+
+import { getClassNamesWithMods } from '../_helpers';
+
+/**
+ * Badge component
+ */
+class Badge extends Component {
+  renderBadge() {
+    const { arrow, border, children, position, title, ...otherProps } = this.props;
+
+    const mods = {
+      'arrow-left': arrow && position === 'right',
+      'arrow-right': arrow && position === 'left',
+      'no-border': !border,
+    };
+
+    if (position && children) {
+      mods[position] = true;
+    }
+
+    return (
+      <div {...otherProps} className={getClassNamesWithMods('ui-badge-badge', mods)}>
+        {title}
+        {arrow && (position === 'right' || position === 'left') ? <span className="ui-badge-badge-arrow" /> : null}
+      </div>
+    );
+  }
+
+  render() {
+    const { children } = this.props;
+
+    if (!children) {
+      return this.renderBadge();
+    }
+
+    return (
+      <div className="ui-badge">
+        {children}
+        {this.renderBadge()}
+      </div>
+    );
+  }
+}
+
+Badge.propTypes = {
+  /**
+   * Does the Badge have an arrow
+   */
+  arrow: PropTypes.bool,
+  /**
+   * Does the Badge have a bottom border
+   */
+  border: PropTypes.bool,
+  /**
+   * Content, that will be wrapped by Badge
+   */
+  children: PropTypes.node,
+  /**
+   * The Badge's position
+   */
+  position: PropTypes.oneOf(['top', 'right', 'bottom', 'left']),
+  /**
+   * The Badge's title
+   */
+  title: PropTypes.string.isRequired,
+};
+
+Badge.defaultProps = {
+  arrow: false,
+  border: true,
+  children: null,
+};
+
+export default Badge;
---
+++ components/badge/badge.md
@@ -0,0 +1,51 @@
+Position top:
+
+    <Badge position="top" title="Badge">
+      <div style={{border: '1px solid lightgray', height: '200px', marginTop: '15px'}} />
+    </Badge>
+
+Position right:
+
+    <Badge position="right" title="Badge">
+      <div style={{border: '1px solid lightgray', height: '200px', marginTop: '15px'}} />
+    </Badge>
+
+Position right with arrow:
+
+    <Badge arrow position="right" title="Badge">
+      <div style={{border: '1px solid lightgray', height: '200px', marginTop: '15px'}} />
+    </Badge>
+
+Position bottom:
+
+    <Badge position="bottom" title="Badge">
+      <div style={{border: '1px solid lightgray', height: '200px', marginTop: '15px'}} />
+    </Badge>
+
+Position left:
+
+    <Badge position="left" title="Badge">
+      <div style={{border: '1px solid lightgray', height: '200px', marginTop: '15px'}} />
+    </Badge>
+
+Position left with arrow:
+
+    <Badge arrow position="left" title="Badge">
+      <div style={{border: '1px solid lightgray', height: '200px', marginTop: '15px'}} />
+    </Badge>
+
+Without content:
+
+    <Badge title="Badge" />
+
+Without content and with right arrow:
+
+    <Badge arrow position="right" title="Badge" />
+
+Without content and with left arrow:
+
+    <Badge arrow position="left" title="Badge" />
+
+Without border:
+
+    <Badge border={false} title="Badge" />
