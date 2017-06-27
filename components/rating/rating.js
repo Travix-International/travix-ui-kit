d252eb9b677221ecaeccc0fd69a686fa1bbe5a04
Carlos da Costa <CCosta@LT-NLAMS-620.local>
2017-06-27 16:29:22 +0200

    add the rating component

---
+++ components/rating/rating.js
@@ -0,0 +1,48 @@
+// Imports
+import React, { PropTypes } from 'react';
+
+import { getDataAttributes } from '../_helpers';
+
+/**
+ * Ratint component
+ */
+function Rating({ rate, dataAttrs, size }) {
+  const restProps = getDataAttributes(dataAttrs);
+
+  const stars = Array.from(Array(size).keys());
+  const value = Math.ceil((rate / size) * 100);
+
+  return (
+    <div className="ui-rating" {...restProps}>
+      {stars.map(v => <b key={v}>★</b>)}
+      <div className="ui-rating-value" style={{ width: `${value}%` }}>
+        {stars.map(v => <b key={v}>★</b>)}
+      </div>
+    </div>
+  );
+}
+
+Rating.propTypes = {
+  /**
+   * Rate percentage
+   */
+  rate: PropTypes.number,
+  /**
+   * Data attribute. You can use it to set up any custom data-* attribute.
+   */
+  dataAttrs: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.object,
+  ]),
+  /**
+   * Size of the rating (how many stars)
+   */
+  size: PropTypes.number,
+};
+
+Rating.defaultProps = {
+  rate: 0,
+  size: 5,
+};
+
+export default Rating;
---
+++ tests/unit/rating/rating.spec.js
@@ -0,0 +1,40 @@
+import { mount } from 'enzyme';
+import React from 'react';
+import Rating from '../../../components/rating/rating';
+
+describe('Rating', () => {
+  describe('#render()', () => {
+    it('renders with default props and mods provided', () => {
+      const renderTree = mount(
+        <Rating rate={0} />
+      );
+
+      const ratingElement = renderTree.find('.ui-rating');
+      const valueElement = ratingElement.find('.ui-rating-value');
+      const ratingStars = ratingElement.find('> b');
+      const valueStars = valueElement.find('> b');
+
+      expect(renderTree).toMatchSnapshot();
+      expect(ratingStars.length).toEqual(5);
+      expect(valueStars.length).toEqual(5);
+      expect(valueElement.node.style.width).toEqual("0%");
+    });
+
+    it('renders with custom props', () => {
+      const size = 10;
+      const renderTree = mount(
+        <Rating rate={1.5} size={size} />
+      );
+
+      const ratingElement = renderTree.find('.ui-rating');
+      const valueElement = ratingElement.find('.ui-rating-value');
+      const ratingStars = ratingElement.find('> b');
+      const valueStars = valueElement.find('> b');
+
+      expect(renderTree).toMatchSnapshot();
+      expect(ratingStars.length).toEqual(size);
+      expect(valueStars.length).toEqual(size);
+      expect(valueElement.node.style.width).toEqual("15%");
+    });
+  });
+});
