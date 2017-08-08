9380a8029b5a34f76c46d555baf30c2d60b8e557
Alina_Piletskaya <rumatadeestor@gmail.com>
2017-08-08 13:37:02 +0300

    added docs

--- components/googleMap/googleMap.js
+++ components/googleMap/googleMap.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import ReactDOM from 'react-dom';
 
+/**
+ * GoogleMap component
+ */
 export default class GoogleMap extends Component {
+  /**
+   * Calls custom marker constructor for further using while creating new markers
+   *
+   * @method constructor
+   * @param {Object} props
+   */
   constructor(props) {
     super(props);
 
@@ -10,10 +19,21 @@ export default class GoogleMap extends Component {
     this.CustomMarker = this.initCustomMarkerConstructor();
   }
 
+  /**
+   * Calls initialize method passing google api and options
+   *
+   * @method componentDidMount
+   */
   componentDidMount() {
     this.initialize(this.props);
   }
 
+  /**
+   * Calls initialize method to reinit a map passing new options
+   *
+   * @method componentWillReceiveProps
+   * @param {Object} nextProps
+   */
   componentWillReceiveProps(nextProps) {
     if (!nextProps.google) {
       return;
@@ -22,12 +42,24 @@ export default class GoogleMap extends Component {
     this.initialize(nextProps);
   }
 
+  /**
+   * Calls passed handler for marker click event if any
+   *
+   * @method handleMarkerClick
+   * @param {Number} index - index of marker from marker array
+   */
   handleMarkerClick = (index) => {
     if (typeof this.props.onMarkerClick === 'function') {
       this.props.onMarkerClick(index);
     }
   }
 
+  /**
+   * Initializes custom marker constructor
+   *
+   * @method initCustomMarkerConstructor
+   * @return {function} CustomMarker constructor
+   */
   initCustomMarkerConstructor() {
     const { google } = this.props;
     const handleCustomMarkerClick = this.handleMarkerClick;
@@ -84,6 +116,17 @@ export default class GoogleMap extends Component {
     return CustomMarker;
   }
 
+  /**
+   * Create normal marker using passed props
+   *
+   * @method createMarker
+   * @param {Object} google - google api object
+   * @param {Object} pointData - contains config for position, marker, info window
+   * @param {Number} index - index of marker from marker array
+   * @param {Object} map - map where to put a new marker
+   * @param {Object} newInfoWindow - info window data
+   * @return {Object} newMarker instance
+   */
   createMarker(google, pointData, index, map, newInfoWindow) {
     const { onMarkerClick } = this.props;
     const { position, marker, info } = pointData;
@@ -122,12 +165,31 @@ export default class GoogleMap extends Component {
     return newMarker;
   }
 
+  /**
+   * Calls Custom Marker constructor and links new marker to `this`
+   *
+   * @method createCustomMarker
+   * @param {Object} google - google api object
+   * @param {Object} pointData - contains config for position, marker, info window
+   * @param {Number} index - index of marker from marker array
+   * @param {Object} map - map where to put a new marker
+   */
   createCustomMarker(google, pointData, index, map) {
     this.overlay = new this.CustomMarker(pointData, map, index);
   }
 
+  /**
+   * Sets up a map with passed options and calls a corresponding methods for creating markers
+   *
+   * @method initialize
+   * @param {Object} options - google api object
+   */
   initialize(options) {
     const { zoom, styles, center, restMapSettings, points, zoomControl, mapTypeControl, google } = options;
+
+    if (!google) {
+      return;
+    }
 
     const settings = {
       zoom,
@@ -157,6 +219,12 @@ export default class GoogleMap extends Component {
     });
   }
 
+  /**
+   * Sets up a map with passed options and calls a corresponding methods for creating markers
+   *
+   * @method linkRef
+   * @param {Object} ref - node object to be linked as a ref; a map will be rendered here
+   */
   linkRef = (ref) => {
     this.mapContainer = ref;
   }
@@ -174,16 +242,52 @@ export default class GoogleMap extends Component {
 }
 
 GoogleMap.propTypes = {
+  // https://developers.google.com/maps/documentation/javascript/style-reference
+  /**
+   * Map center coordinates.
+   */
   center: PropTypes.object,
-  google: PropTypes.object,
+  /**
+   * Special google api object.
+   */
+  google: PropTypes.object.isRequired,
+  /**
+   * The height of map container.
+   */
   height: PropTypes.string,
+  /**
+   * Visibility of map type control.
+   */
   mapTypeControl: PropTypes.bool,
+  /**
+   * Optional onClick handler for markers.
+   */
   onMarkerClick: PropTypes.func,
+  /**
+   * Points coordinates.
+   */
   points: PropTypes.arrayOf(PropTypes.object),
+  /**
+   * Any other properies Google API allows for customization.
+   */
   restMapSettings: PropTypes.object,
+  /**
+   * Object which contains settings for custom style.
+   * It has special format.
+   * Please, check docs: https://developers.google.com/maps/documentation/javascript/style-reference
+   */
   styles: PropTypes.arrayOf(PropTypes.object),
+  /**
+   * The width of map container.
+   */
   width: PropTypes.string,
+  /**
+   * The initial resolution at which to display the map. As a rule, from 0 to 18.
+   */
   zoom: PropTypes.number,
+  /**
+   * Visibility of zoom control.
+   */
   zoomControl: PropTypes.bool,
 };
 
