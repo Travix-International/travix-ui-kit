2d07c7bf9c352058cb2d32cc1c5d40a2b044a543
Ricardo Machado <pt.n00b@gmail.com>
2017-09-11 22:40:17 +0200

    Improves the overlayTrigger's unit tests to be back to 100% coverage (#173)

--- tests/unit/overlayTrigger/overlayTrigger.showElement.spec.js
+++ tests/unit/overlayTrigger/overlayTrigger.showElement.spec.js
@@ -1,18 +1,37 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+
 import OverlayTrigger from '../../../components/overlayTrigger/overlayTrigger';
 
 describe('OverlayTrigger: showElement', () => {
-  it('should set state as active and call onElementShow callback if it exists', () => {
-    const onElementShow = jest.fn();
-    const prototype = {
-      props: {
-        onElementShow,
-      },
-      setState: jest.fn(),
+  it('should set state as active and call the default prop onElementShow', () => {
+    const props = {
+      children: <div className="test-child"/>,
+      elemToToggle: <div className="test-toogle-element"/>,
     };
 
-    OverlayTrigger.prototype.showElement.call(prototype);
+    const wrapper = shallow(<OverlayTrigger {...props}>test</OverlayTrigger>);
+    const targetElement = wrapper.find('.ui-overlay-trigger__content > div').at(0);
 
-    expect(prototype.setState).toBeCalledWith({ active: true });
-    expect(onElementShow).toBeCalled();
+    expect(wrapper.state().active).toBe(false);
+
+    targetElement.simulate('click');
+
+    expect(wrapper.state().active).toBe(true);
+  });
+
+  it('should call onElementShow callback passed as prop', () => {
+    const props = {
+      children: <div className="test-child"/>,
+      elemToToggle: <div className="test-toogle-element"/>,
+      onElementShow: jest.fn(),
+    };
+
+    const wrapper = shallow(<OverlayTrigger {...props}>test</OverlayTrigger>);
+    const targetElement = wrapper.find('.ui-overlay-trigger__content > div').at(0);
+
+    targetElement.simulate('click');
+
+    expect(props.onElementShow).toHaveBeenCalled();
   });
 });
