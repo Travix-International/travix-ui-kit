2df3cccd5af2e42cd867a2a16e43fff615c4e682
Ricardo Machado <rmachado@travix.com>
2017-09-11 16:48:20 +0200

    Improves the overlayTrigger's unit tests to be back to 100% coverage

--- tests/unit/overlayTrigger/overlayTrigger.hideElement.spec.js
+++ tests/unit/overlayTrigger/overlayTrigger.hideElement.spec.js
@@ -1,18 +1,39 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+
 import OverlayTrigger from '../../../components/overlayTrigger/overlayTrigger';
 
 describe('OverlayTrigger: hideElement', () => {
-  it('should set state as inactive and call onElementHide callback if it exists', () => {
-    const onElementHide = jest.fn();
-    const prototype = {
-      props: {
-        onElementHide,
-      },
-      setState: jest.fn(),
+  it('should set state as inactive and call the default prop onElementHide', () => {
+    const props = {
+      children: <div className="test-child"/>,
+      elemToToggle: <div className="test-toogle-element"/>,
     };
 
-    OverlayTrigger.prototype.hideElement.call(prototype);
+    const wrapper = shallow(<OverlayTrigger {...props}>test</OverlayTrigger>);
+    const targetElement = wrapper.find('.ui-overlay-trigger__content > div').at(0);
 
-    expect(prototype.setState).toBeCalledWith({ active: false });
-    expect(onElementHide).toBeCalled();
+    wrapper.instance().showElement();
+    expect(wrapper.state().active).toBe(true);
+
+    targetElement.simulate('click');
+
+    expect(wrapper.state().active).toBe(false);
+  });
+
+  it('should call onElementHide callback passed as prop', () => {
+    const props = {
+      children: <div className="test-child"/>,
+      elemToToggle: <div className="test-toogle-element"/>,
+      onElementHide: jest.fn(),
+    };
+
+    const wrapper = shallow(<OverlayTrigger {...props}>test</OverlayTrigger>);
+    const targetElement = wrapper.find('.ui-overlay-trigger__content > div').at(0);
+
+    wrapper.instance().showElement();
+    targetElement.simulate('click');
+
+    expect(props.onElementHide).toHaveBeenCalled();
   });
 });
