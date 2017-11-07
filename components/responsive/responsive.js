ef8d71fcb138d148b411a0804f24825540293294
Artem Riasnianskyi <asci@yandex.ru>
2017-11-07 12:27:35 +0000

    POC for behavioral component Responsive

---
+++ components/responsive/responsive.js
@@ -0,0 +1,146 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import sizes from 'travix-breakpoints/sizes';
+import debounce from 'lodash.debounce';
+
+const sizesNumbers = Object.keys(sizes).reduce((base, key) => {
+  base[key] = {
+    min: Number(sizes[key].min.replace('px', '')),
+    max: Number(sizes[key].max.replace('px', '')),
+  };
+
+  return base;
+}, {});
+
+const ignoreFrameSize = {
+  min: 0,
+  max: 9999,
+};
+
+/**
+ * Behaviour component for responsive. Renders or doesn't render children depending on the screen size
+ */
+export default class Responsive extends Component {
+  static propTypes = {
+    /**
+     * Children to show or hide
+     */
+    children: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.element,
+      PropTypes.node,
+    ]).isRequired,
+    /**
+     * Show if screen size is greater then
+     */
+    gt: PropTypes.oneOf([
+      'tiny',
+      'small',
+      'meduim',
+      'large',
+      'xlarge',
+    ]),
+    /**
+     * Show if screen size is less then
+     */
+    lt: PropTypes.oneOf([
+      'tiny',
+      'small',
+      'meduim',
+      'large',
+      'xlarge',
+    ]),
+    /**
+     * Show if screen size is exact
+     */
+    only: PropTypes.oneOf([
+      'tiny',
+      'small',
+      'meduim',
+      'large',
+      'xlarge',
+    ]),
+    /**
+     * If you render a string you need a wrapper element, like `div` or `span`
+     */
+    wrap: PropTypes.element,
+  };
+
+  static defaultProps = {
+    wrap: 'span',
+  };
+
+  static getState = (props) => {
+    const { gt, lt, only } = props;
+    let frameSize = sizesNumbers[only || gt || lt];
+
+    if (!frameSize) {
+      console.warn(`Responsive: Passed unknown frame size name: ${only || gt || lt}. Ignoring any window width`);
+      frameSize = ignoreFrameSize;
+    }
+
+    if ((gt && lt) || (gt && only) || (lt && only)) {
+      console.warn('Responsive: Passed more than one frame size. `only` has a priority');
+    }
+
+    return {
+      width: window.innerWidth,
+      min: frameSize.min,
+      max: frameSize.max,
+    };
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = Responsive.getState(props);
+    this.handleResize = this.handleResize.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.state = Responsive.getState(nextProps);
+  }
+
+  componentDidMount() {
+    this.bindEvents();
+  }
+
+  componentWillUnmount() {
+    this.unbindEvents();
+  }
+
+  bindEvents = () => {
+    window.addEventListener('resize', this.handleUpdate);
+  }
+
+  unbindEvents = () => {
+    window.removeEventListener('resize', this.handleUpdate);
+  }
+
+  isVisible = () => {
+    const { width, max, min } = this.state;
+
+    return (width >= min) && (width <= max);
+  }
+
+  handleResize = debounce(() => {
+    requestAnimationFrame(this.handleUpdate);
+  }, 300)
+
+  handleUpdate = () => {
+    this.setState({ width: window.innerWidth });
+  }
+
+  render() {
+    const { children, wrap } = this.props;
+
+    if (this.isVisible()) {
+      if (typeof children === 'string') {
+        return React.createElement(wrap, null, children);
+      }
+
+      return children;
+    }
+
+    return null;
+  }
+}
---
+++ components/responsive/responsive.md
@@ -0,0 +1,10 @@
+Sizes:
+
+    <div>
+      <h1>Change width of the window to see the difference</h1>
+      <Responsive only="tiny">tiny</Responsive>
+      <Responsive only="small">small</Responsive>
+      <Responsive only="medium">medium</Responsive>
+      <Responsive only="large">large</Responsive>
+      <Responsive only="xlarge">xlarge</Responsive>
+    </div>
