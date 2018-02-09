a47c3e678f781d6cb4c97fce5a059b73473cb5cf
Aliaksandr Kavalenka <Aliaksandr_Kavalenka@epam.com>
2018-02-09 13:34:30 +0300

    update unit tests

--- tests/unit/modal/modal.componentWillReceiveProps.spec.js
+++ tests/unit/modal/modal.componentWillReceiveProps.spec.js
@@ -21,7 +21,7 @@ describe('Modal: componentWillReceiveProps', () => {
     expect(Modal.prototype.open).toBeCalled();
 
     component.setProps({ active: false });
-    expect(Modal.prototype.close).toBeCalled();
+    setTimeout(() => expect(Modal.prototype.close).toBeCalled(), 0);
   });
 
   it('should not open/close modal when "active" prop was not changed', () => {
@@ -41,5 +41,24 @@ describe('Modal: componentWillReceiveProps', () => {
     component.setProps({ active: false });
     expect(Modal.prototype.open).not.toBeCalled();
     expect(Modal.prototype.close).not.toBeCalled();
+  });
+
+  it('should not close modal when "active" is false', () => {
+    Modal.prototype.open = jest.fn();
+    Modal.prototype.close = jest.fn();
+
+    const component = shallow(
+      <Modal
+        active
+      >
+        Modal Content
+      </Modal>
+    );
+
+    expect(Modal.prototype.close).not.toBeCalled();
+    component.find('.ui-modal__overlay').simulate('click');
+
+    component.setProps({ active: false });
+    setTimeout(() => expect(Modal.prototype.close).not.toBeCalled(), 0);
   });
 });
