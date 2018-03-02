be89f77e956306c9e81f69ecf8de5b9348a493d8
leandrooriente <leandroriente@gmail.com>
2018-03-02 19:53:06 +0100

    Fix tests

--- tests/unit/toggleButton/toggleButton.spec.js
+++ tests/unit/toggleButton/toggleButton.spec.js
@@ -1,9 +1,10 @@
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import React from 'react';
 import ToggleButton from '../../../components/toggleButton/toggleButton';
+import ToggleItem from '../../../components/toggleButton/toggleItem';
 
 describe('ToggleButton', () => {
-  it('renders null when no items are provided to ToggleButton', () => {
+  it('renders null when no items or children are provided to ToggleButton', () => {
     const wrapper = shallow(<ToggleButton />);
     expect(wrapper.type()).toEqual(null);
   });
@@ -14,12 +15,24 @@ describe('ToggleButton', () => {
       'Second item',
       'Third item',
     ];
-    const wrapper = shallow(<ToggleButton items={items} />);
+    const wrapper = mount(<ToggleButton items={items} />);
 
     const selectedItem = wrapper.find('.ui-toggle-button__item_active');
 
     expect(wrapper).toMatchSnapshot();
     expect(selectedItem.text()).toEqual(items[0]);
+  });
+
+  it('renders the ToggleButton component using ToggleItems', () => {
+    const wrapper = mount(
+      <ToggleButton>
+        <ToggleItem>First Item</ToggleItem>
+        <ToggleItem>Second Item</ToggleItem>
+        <ToggleItem>Third Item</ToggleItem>
+      </ToggleButton>
+    );
+
+    expect(wrapper).toMatchSnapshot();
   });
 
   it('passing a value in selectedIndex will select the proper index of the list of items', () => {
@@ -29,13 +42,13 @@ describe('ToggleButton', () => {
       'Third item',
     ];
     const fakeEvent = { stopPropagation: jest.fn() };
-    const wrapper = shallow(<ToggleButton items={items} selectedIndex={2} />);
+    const wrapper = mount(<ToggleButton items={items} selectedIndex={2} />);
     const selectedItem = wrapper.find('.ui-toggle-button__item_active');
 
     expect(wrapper).toMatchSnapshot();
     expect(selectedItem.text()).toEqual(items[2]);
 
-    wrapper.find('.ui-toggle-button__item').at(0).simulate('click', fakeEvent);
+    wrapper.find(ToggleItem).first().simulate('click', fakeEvent);
     expect(fakeEvent.stopPropagation).toHaveBeenCalledTimes(1);
   });
 
@@ -49,13 +62,11 @@ describe('ToggleButton', () => {
     const mockedHandleSelect = jest.fn();
     const fakeEvent = { stopPropagation: jest.fn() };
 
-    const wrapper = shallow(<ToggleButton handleSelect={mockedHandleSelect} items={items} />);
-
-    wrapper.find('.ui-toggle-button__item').at(0).simulate('click', fakeEvent);
+    const wrapper = mount(<ToggleButton handleSelect={mockedHandleSelect} items={items} />);
+    wrapper.find(ToggleItem).first().simulate('click', fakeEvent);
 
     expect(wrapper).toMatchSnapshot();
     expect(fakeEvent.stopPropagation).toHaveBeenCalledTimes(1);
     expect(mockedHandleSelect).toHaveBeenCalledTimes(1);
-    expect(mockedHandleSelect).toBeCalledWith(fakeEvent, 0);
   });
 });
