a7afc7bacfe49641c9cf42a84fd22c814bb0bd15
leandrooriente <leandroriente@gmail.com>
2018-03-03 11:18:09 +0100

    Fix some unit tests to focus on new approach

--- tests/unit/toggleButton/toggleButton.spec.js
+++ tests/unit/toggleButton/toggleButton.spec.js
@@ -4,18 +4,20 @@ import ToggleButton from '../../../components/toggleButton/toggleButton';
 import ToggleItem from '../../../components/toggleButton/toggleItem';
 
 describe('ToggleButton', () => {
+  const defaultMockedHandleSelect = jest.fn();
+
   it('renders null when no items or children are provided to ToggleButton', () => {
-    const wrapper = shallow(<ToggleButton />);
+    const wrapper = shallow(<ToggleButton handleSelect={defaultMockedHandleSelect} />);
     expect(wrapper.type()).toEqual(null);
   });
 
-  it('renders the ToggleButton component with the defaults', () => {
+  it('renders the ToggleButton component using items', () => {
     const items = [
       'First item',
       'Second item',
       'Third item',
     ];
-    const wrapper = mount(<ToggleButton items={items} />);
+    const wrapper = mount(<ToggleButton handleSelect={defaultMockedHandleSelect} items={items} />);
 
     const selectedItem = wrapper.find('.ui-toggle-button__item_active');
 
@@ -23,9 +25,9 @@ describe('ToggleButton', () => {
     expect(selectedItem.text()).toEqual(items[0]);
   });
 
-  it('renders the ToggleButton component using ToggleItems', () => {
+  it('renders the ToggleButton component with the defaults', () => {
     const wrapper = mount(
-      <ToggleButton>
+      <ToggleButton handleSelect={defaultMockedHandleSelect}>
         <ToggleItem>First Item</ToggleItem>
         <ToggleItem>Second Item</ToggleItem>
         <ToggleItem>Third Item</ToggleItem>
@@ -36,20 +38,17 @@ describe('ToggleButton', () => {
   });
 
   it('passing a value in selectedIndex will select the proper index of the list of items', () => {
-    const items = [
-      'First item',
-      'Second item',
-      'Third item',
-    ];
-    const fakeEvent = { stopPropagation: jest.fn() };
-    const wrapper = mount(<ToggleButton items={items} selectedIndex={2} />);
+    const wrapper = mount(
+      <ToggleButton handleSelect={defaultMockedHandleSelect} selectedIndex={2}>
+        <ToggleItem>First Item</ToggleItem>
+        <ToggleItem>Second Item</ToggleItem>
+        <ToggleItem>Third Item</ToggleItem>
+      </ToggleButton>
+    );
     const selectedItem = wrapper.find('.ui-toggle-button__item_active');
 
     expect(wrapper).toMatchSnapshot();
-    expect(selectedItem.text()).toEqual(items[2]);
-
-    wrapper.find(ToggleItem).first().simulate('click', fakeEvent);
-    expect(fakeEvent.stopPropagation).toHaveBeenCalledTimes(1);
+    expect(selectedItem.text()).toEqual('Third Item');
   });
 
   it('when a "handleSelect" function is passed it gets executed when clicking on an option', () => {
@@ -59,14 +58,13 @@ describe('ToggleButton', () => {
       'Third item',
     ];
 
-    const mockedHandleSelect = jest.fn();
     const fakeEvent = { stopPropagation: jest.fn() };
 
-    const wrapper = mount(<ToggleButton handleSelect={mockedHandleSelect} items={items} />);
+    const wrapper = mount(<ToggleButton handleSelect={defaultMockedHandleSelect} items={items} />);
     wrapper.find(ToggleItem).first().simulate('click', fakeEvent);
 
     expect(wrapper).toMatchSnapshot();
     expect(fakeEvent.stopPropagation).toHaveBeenCalledTimes(1);
-    expect(mockedHandleSelect).toHaveBeenCalledTimes(1);
+    expect(defaultMockedHandleSelect).toHaveBeenCalledTimes(1);
   });
 });
